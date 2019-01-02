6963503574f772df352486bf3cefd61697e616e7
Jeevanandam M <jeeva@myjeeva.com>
2019-01-01 22:42:46 -0800

    initial check-in of resty website with tiny home page

---
+++ app/init.go
@@ -0,0 +1,82 @@
+// aah application initialization - configuration, server extensions, middleware's, etc.
+// Customize it per application use case.
+
+package main
+
+import (
+	"aahframe.work"
+
+	// Registering HTML minifier
+	_ "aahframe.work/minify/html"
+)
+
+func init() {
+	app := aah.App()
+
+	//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+	// Server Extensions
+	// Doc: https://docs.aahframework.org/server-extension.html
+	//__________________________________________________________________________
+	app.OnStart(SubscribeHTTPEvents)
+
+	//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+	// Middleware's
+	// Doc: https://docs.aahframework.org/middleware.html
+	//
+	// Executed in the order they are defined. It is recommended; NOT to change
+	// the order of pre-defined aah framework middleware's.
+	//__________________________________________________________________________
+	app.HTTPEngine().Middlewares(
+		aah.RouteMiddleware,
+		aah.CORSMiddleware,
+		aah.BindMiddleware,
+		aah.AntiCSRFMiddleware,
+		aah.AuthcAuthzMiddleware,
+
+		//
+		// NOTE: Register your Custom middleware's right here
+		//
+
+		aah.ActionMiddleware,
+	)
+
+	//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+	// Add Application Error Handler
+	// Doc: https://docs.aahframework.org/error-handling.html
+	//__________________________________________________________________________
+	// app.SetErrorHandler(AppErrorHandler)
+
+	//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+	// Add Custom Template Functions
+	// Doc: https://docs.aahframework.org/template-funcs.html
+	//__________________________________________________________________________
+	// app.AddTemplateFunc(template.FuncMap{
+	// // ...
+	// })
+
+	//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+	// Add Custom Session Store
+	// Doc: https://docs.aahframework.org/session.html
+	//__________________________________________________________________________
+	// app.AddSessionStore("redis", &RedisSessionStore{})
+
+	//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+	// Add Custom Value Parser
+	// Doc: https://docs.aahframework.org/request-parameters-auto-bind.html
+	//__________________________________________________________________________
+	// if err := app.AddValueParser(reflect.TypeOf(CustomType{}), customParser); err != nil {
+	//   log.Error(err)
+	// }
+}
+
+// SubscribeHTTPEvents method subscribes to HTTP events on app start.
+// Doc: https://docs.aahframework.org/server-extension.html
+func SubscribeHTTPEvents(_ *aah.Event) {
+	// he := aah.App().HTTPEngine()
+	// he.OnRequest(myserverext.OnRequest)
+	// he.OnPreReply(myserverext.OnPreReply)
+	// he.OnHeaderReply(myserverext.OnHeaderReply)
+	// he.OnPostReply(myserverext.OnPostReply)
+	// he.OnPreAuth(myserverext.OnPreAuth)
+	// he.OnPostAuth(myserverext.PostAuthEvent)
+}
---
+++ go.mod
@@ -0,0 +1,6 @@
+module resty
+
+require (
+	aahframe.work v0.12.2
+	aahframe.work/minify/html v0.2.0
+)
